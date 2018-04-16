f7be16c51e6032a96899807c6d0e9dec21def057
Sergey Grebennikov <vkaser@yandex.com>
2018-04-16 10:48:42 +0300

    Task is solved

---
+++ Task_6_1_8/Task_6_1_8/ICloneable.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+struct ICloneable
+{
+	virtual ICloneable* clone() const = 0;
+	virtual ~ICloneable() { }
+};
+
+template <typename T>
+struct ValueHolder : ICloneable {
+	ValueHolder(const T & data) : data_(data)
+	{}
+
+	ValueHolder * clone() const{
+		return new ValueHolder(data_);
+	}
+
+	T data_;
+};
--- Task_6_1_8/Task_6_1_8/main.cpp
+++ Task_6_1_8/Task_6_1_8/main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
+#include "ICloneable.h"
 using namespace std;
 
 int main() {
+	ICloneable * p = new ValueHolder<int>(20);
+	p->clone();
 	return 0;
 }
