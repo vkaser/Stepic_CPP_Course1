354d9e18711469bea07c511c9285bb0ebbe033c9
Sergey Grebennikov <vkaser@yandex.com>
2018-04-29 15:31:06 +0300

    Task solved

---
+++ Task_6_4_3/Task_6_4_3/Source.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+
+template <typename T, size_t size>
+size_t array_size(T (&a)[size]) {
+	return size;
+}
+
+int main() {
+	int ints[] = { 1, 2, 3, 4 };
+	int *iptr = ints;
+	double doubles[] = { 3.14 };
+	std::cout << array_size(ints) << std::endl;
+	std::cout << array_size(doubles) << std::endl;
+	//array_size(iptr); // Compilation error
+	return 0;
+}
