4864191746597a001f0026a8b2400875a07c7e83
Sergey Grebennikov <vkaser@yandex.com>
2018-06-01 09:13:55 +0300

    Task solved

---
+++ Task_5_3_3/Task_5_3_3/Source.cpp
@@ -0,0 +1,51 @@
+﻿struct Expression;
+struct Number;
+struct BinaryOperation;
+
+struct ScopedPtr
+{
+	explicit ScopedPtr(Expression *);
+	~ScopedPtr();
+	Expression* get() const;
+	Expression* release();
+	void reset(Expression *);
+	Expression& operator*() const;
+	Expression* operator->() const;
+
+
+private:
+	ScopedPtr(const ScopedPtr&);
+	ScopedPtr& operator=(const ScopedPtr&);
+
+	Expression *ptr_;
+};
+
+ScopedPtr::ScopedPtr(Expression *ptr = 0) : ptr_(ptr)
+{}
+
+ScopedPtr::~ScopedPtr() {
+	delete ptr_;
+}
+
+Expression* ScopedPtr::get() const {
+	return ptr_;
+}
+
+Expression* ScopedPtr::release() {
+	Expression * pointer = ptr_;
+	ptr_ = nullptr;
+	return pointer;
+}
+
+void ScopedPtr::reset(Expression *ptr = 0) {
+	delete ptr_;
+	ptr_ = ptr;
+}
+
+Expression& ScopedPtr::operator*() const {
+	return *ptr_;
+}
+
+Expression* ScopedPtr::operator->() const {
+	return ptr_;
+}
