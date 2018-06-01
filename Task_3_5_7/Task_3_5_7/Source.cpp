8d4d0ba10e6e99bf6f7dd66c273513c637aff2aa
Sergey Grebennikov <vkaser@yandex.com>
2018-06-01 08:40:54 +0300

    Task solved

---
+++ Task_3_5_7/Task_3_5_7/Source.cpp
@@ -0,0 +1,21 @@
+﻿ struct Cls {
+ Cls(char c, double d, int i);
+ private:
+     char c;
+     double d;
+     int i;
+ };
+
+
+
+char &get_c(Cls &cls) {
+	return *((char *)&cls);
+}
+
+double &get_d(Cls &cls) {
+	return *((double *)&cls + 1);
+}
+
+int &get_i(Cls &cls) {
+	return *((int *)&cls + 4);
+}
