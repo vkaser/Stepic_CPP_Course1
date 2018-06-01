b67072367a0049d8fd6126dcf576ec0019e2fcdb
Sergey Grebennikov <vkaser@yandex.com>
2018-06-01 08:38:52 +0300

    Task solved

---
+++ Task_3_3_12/Task_3_3_12/Source.cpp
@@ -0,0 +1,17 @@
+﻿#include <cstddef> // size_t
+#include <cstring> // strlen, strcpy
+
+struct String {
+	String(const char *str = "");
+	String(size_t n, char c);
+	~String();
+
+	void append(String &other) {
+		char *new_str = strcat(strcpy(new char[(size = size + other.size) + 1], str), other.str);
+		delete[] str;
+		str = new_str;
+	}
+
+	size_t size;
+	char *str;
+};
